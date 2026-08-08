a514fb372a6810f93ec17ac62067a678c5de37b1
G Ajeet <55391283+gajeet11000@users.noreply.github.com>
2026-08-08 14:46:48 +0530

    added buffer close other command

--- dotfiles/nvim/.config/nvim/lua/plugins/bufferline.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -9,7 +9,8 @@ return {
     },
   },
   keys = {
-    {"<S-h>", "<cmd>BufferLineCyclePrev<CR>", { desc = "Prev Buffer" }},
-    {"<S-l>", "<cmd>BufferLineCycleNext<CR>", { desc = "Next Buffer" }},
+    { "<S-h>",      "<cmd>BufferLineCyclePrev<CR>",   { desc = "Prev Buffer" } },
+    { "<S-l>",      "<cmd>BufferLineCycleNext<CR>",   { desc = "Next Buffer" } },
+    { "<leader>bo", "<cmd>BufferLineCloseOthers<CR>", { desc = "Close Other Buffers" } },
   },
 }
